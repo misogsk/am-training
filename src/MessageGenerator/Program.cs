e74474b400e469b4d22d552dc0d36222e71a5af5
michal grezdo <michal.grezdo@accenture.com>
2020-10-22 16:25:32 +0200

    Added message generator

---
+++ src/MessageGenerator/Program.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Azure.ServiceBus;
+using Newtonsoft.Json;
+using System;
+using System.Text;
+
+namespace MessageGenerator
+{
+    class Program
+    {
+        static async void Main(string[] args)
+        {
+            var busClient = new TopicClient("connectionstring", "topic");
+
+            //p.DeliveryKardex = 24181;
+            var dto = new
+            {
+                name = "test"
+            };
+            await busClient.SendAsync(CreateServiceBusMessage(dto));
+        }
+
+        private static Message CreateServiceBusMessage(object messageData)
+        {
+            var serializedPackage = JsonConvert.SerializeObject(messageData);
+            var message = new Message(Encoding.UTF8.GetBytes(serializedPackage));
+            return message;
+        }
+
+    }
+}
